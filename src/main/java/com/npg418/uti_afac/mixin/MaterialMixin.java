abe9ef9f55cc1a74a17b275cfb5f1d2871c379a1
NPG418 <narupenguin919@gmail.com>
2024-11-01 23:30:08 +0900

    gtceuのmaterialに対応

---
+++ src/main/java/com/npg418/uti_afac/mixin/MaterialMixin.java
@@ -0,0 +1,21 @@
+package com.npg418.uti_afac.mixin;
+
+import bre2el.uti.UntranslatedItems;
+import bre2el.uti.hook.FluidTypeHook;
+import com.gregtechceu.gtceu.api.data.chemical.material.Material;
+import net.minecraft.network.chat.MutableComponent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Material.class)
+public class MaterialMixin {
+    @Inject(method = "getLocalizedName", at = @At("RETURN"), cancellable = true, remap = false)
+    public void switchLangMapForMaterial(CallbackInfoReturnable<MutableComponent> cir) {
+        if (UntranslatedItems.initComplete) {
+            FluidTypeHook.getDescriptionPreHook();
+            cir.setReturnValue(FluidTypeHook.getDescriptionHook(cir.getReturnValue()).copy());
+        }
+    }
+}
