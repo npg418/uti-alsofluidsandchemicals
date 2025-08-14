84906439cb5001fb8b93674482b8a7c7d4dc062f
NPG418 <narupenguin919@gmail.com>
2025-08-14 17:41:16 +0900

    EmiのFluidStackに対応

---
+++ src/main/java/com/npg418/uti_afac/mixin/FluidEmiStackMixin.java
@@ -0,0 +1,36 @@
+package com.npg418.uti_afac.mixin;
+
+import com.npg418.uti_afac.util.TooltipAppender;
+import dev.emi.emi.api.stack.FluidEmiStack;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraftforge.fluids.FluidStack;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("UnstableApiUsage")
+@Mixin(value = FluidEmiStack.class, remap = false)
+public class FluidEmiStackMixin {
+    @Shadow
+    @Final
+    private Fluid fluid;
+
+    @Final
+    @Shadow
+    private CompoundTag nbt;
+
+    @Inject(method = "getTooltipText", at = @At("RETURN"), cancellable = true)
+    private void appendSecondaryLanguageToTooltip(CallbackInfoReturnable<List<Component>> cir) {
+        List<Component> tooltip = new ArrayList<>(cir.getReturnValue());
+        TooltipAppender.appendTooltip(tooltip, () -> new FluidStack(fluid, 1000, nbt).getDisplayName());
+        cir.setReturnValue(tooltip);
+    }
+}
